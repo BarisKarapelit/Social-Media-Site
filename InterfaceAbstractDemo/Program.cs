a5a790ee6193e081a4d15e8d4070ccc2e1d78a90
BarisKarapelit <bkarapelit48@gmail.com>
2022-08-03 18:45:13 +0300

    Added the Library files

---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAbstractDemo
+{
+    internal class Program
+    {
+       public static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new NoreCustomerManager(new MernisServiceAdapter());
+            customerManager.Save(new Customer { 
+                DateOfBirth=new DateTime(1995,8,22),
+                FirstName="Barış",
+                LastName="Karapelit",
+                NationalityId= "59920514432"
+            });
+            Console.ReadLine();
+
+        }
+    }
+}
