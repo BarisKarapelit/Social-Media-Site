a4468c7117568b0a299816bbd624e559b93e4adf
SmartPro305 <kemes53247@nifect.com>
2022-08-10 12:09:24 +0300

    Camp 20 Table Usage

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,33 @@
+﻿using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+   public class CategoryManager
+    {
+        GenericRepository<Category> category = new GenericRepository<Category>();
+
+        public List<Category> GetAll()
+        {
+            return category.List();
+        }
+        public void CategoryAddBl(Category p)
+        {
+            bool query = (p.CategoryName == ""|| p.CategoryName.Length <= 3 || p.CategoryDescription == "" || p.CategoryName.Length >= 51);
+
+            if (query)
+            {
+
+            }
+            else
+            {
+                category.Insert(p);
+            }
+        }
+    }
+}
