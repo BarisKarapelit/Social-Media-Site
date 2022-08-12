c596537b6d35b60835c55edb7760cf03be03d0dc
SmartPro305 <kemes53247@nifect.com>
2022-08-12 11:38:10 +0300

    Fluent Validation

--- BusinessLayer/ValidationRules/CategoryValidator.cs
+++ BusinessLayer/ValidationRules/CategoryValidator.cs
@@ -8,9 +8,9 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.ValidationRules
 {
-   public class CategoryValidatior:AbstractValidator<Category>
+   public class CategoryValidator:AbstractValidator<Category>
     {
-        public CategoryValidatior()
+        public CategoryValidator()
         {
             RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Kategori Adını Boş Geçemezsiniz");
             RuleFor(x => x.CategoryDescription).NotEmpty().WithMessage("Kategori Açıklaması Boş Geçemezsiniz");
